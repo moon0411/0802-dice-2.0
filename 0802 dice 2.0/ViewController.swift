13ea8499411be0715cd26c02870390c74c73ac9e
moon <wings0411@gmail.com>
2018-08-02 12:23:43 +0800

    Initial Commit

---
+++ 0802 dice 2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  0802 dice 2.0
+//
+//  Created by 李沐軒 on 2018/8/2.
+//  Copyright © 2018年 李沐軒. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
